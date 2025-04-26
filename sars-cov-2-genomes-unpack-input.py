b049bbc9315e4f01610abf13a81e29b8fdcf0ef8
Mike Honey <mike.honey@mangasolutions.com>
2025-04-26 23:51:56 +1000

    reading metadata.tsv in chunks of 1M rows
    to reduce memory pressure

--- sars-cov-2-genomes-unpack-input.py
+++ sars-cov-2-genomes-unpack-input.py
@@ -31,10 +31,21 @@ def main():
             tar.close()
             print (str(datetime.datetime.now()) + ' Finished unzip of ' + filename )
 
-    metadata_latest = pandas.read_csv(datadir + 'metadata.tsv', delimiter='\t')
+    # initialise a list to hold chunks of the extracted tsv file (13GB+)
+    metadata_latest = [] 
+
+    # read the extracted tsv file in chunks of 1M rows and process each chunk
+    with pandas.read_csv(datadir + 'metadata.tsv', delimiter='\t', iterator=True, chunksize=1000000) as metadata_tsv:
     # Filter the data accordingly.
-    metadata_latest = metadata_latest[metadata_latest['Collection date'] > '2023-12-22']
-    metadata_latest.to_csv(datadir + 'metadata_latest.tsv', sep='\t')
+        print (str(datetime.datetime.now()) + ' Filtering by Collection date ...' )
+        for metadata_chunk in metadata_tsv:
+            print (str(datetime.datetime.now()) + ' Processing a 1M row chunk of metadata.tsv ...' )
+            # filter each chunk and append the resulting dataframe to the list.
+            metadata_latest.append(metadata_chunk[metadata_chunk['Collection date'] > '2023-12-22'])
+
+    print (str(datetime.datetime.now()) + ' Writing metadata_latest.tsv ...' )
+    # concatenate all the filtered dataframes together and write to the output file.
+    pandas.concat(metadata_latest).to_csv(datadir + 'metadata_latest.tsv', sep='\t')
     print (str(datetime.datetime.now()) + ' Finished writing of metadata_latest.tsv' )
 
 
